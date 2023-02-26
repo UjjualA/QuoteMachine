4681ca9cf3b797094dcd78447484af5c3be1ef35
Ujjual <113343711+UjjualA@users.noreply.github.com>
2023-02-26 18:50:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quotes from './components/Quotes';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Quotes/>
+    </div>
+  )
+}
+export default App
